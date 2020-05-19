7dff3363627d70db238ab4938d26a82093539c0f
nilay <mailnilaydas@gmail.com>
2020-05-19 12:31:21 +0530

    commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import org.example.model.Hello;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( new Hello().helloMessage("John"));
     }
 }
